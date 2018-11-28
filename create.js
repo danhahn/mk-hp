e78734d143fef8c2b69a2158b0dcc4fbbf223249
Dan Hahn <daniel.hahn@michaelkors.com>
2018-11-28 13:47:54 -0500

    add image sizes to json

--- create.js
+++ create.js
@@ -7,7 +7,7 @@ let url = "https://www.michaelkors.com/";
 (async () => {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  await page.setViewport({ width: 1200, height: 500 });
+  await page.setViewport({ width: 414, height: 500 });
   await page.goto(url);
 
   // get hotel details
@@ -19,7 +19,7 @@ let url = "https://www.michaelkors.com/";
     const topBannerDOM = document.querySelector('.flagshipSaleBanner');
     const topBanner = {
       "banner_url": topBannerDOM.querySelector('a').href,
-      "banner_img": `https:${topBannerDOM.querySelector('[srcset*=mobile]').srcset}`,
+      "banner_img": topBannerDOM.querySelector('[srcset*=mobile]').srcset,
       "banner_img_height": "358",
       "banner_img_width": "388",
       "caption--headline": topBannerDOM.querySelector('h2').textContent.trim(),
@@ -40,8 +40,8 @@ let url = "https://www.michaelkors.com/";
       return {
         "mmk_url": item.querySelector('a').href,
         "mmk_img": item.querySelector("[srcset *= 'mobile']").srcset,
-        "mmk_img_width": "320",
-        "mmk_img_height": "320",
+        "mmk_img_width": Math.round(item.querySelector('img').getBoundingClientRect().width),
+        "mmk_img_height": Math.round(item.querySelector('img').getBoundingClientRect().height),
         "mmk_Headline": item.querySelector('.caption--headline').textContent.trim(),
         "font_family": [...item.querySelector('.caption').classList].join(' '),
         "mmk_subtext": item.querySelector('.caption--headline + div').textContent.trim()
@@ -50,13 +50,13 @@ let url = "https://www.michaelkors.com/";
     const slider = [...document.querySelector('.carousel').querySelectorAll('figure.promo')].map(item => {
       return {
         "slider_img": item.querySelector("[srcset *= 'mobile']").srcset,
-        "slider_img_height": "320",
-        "slider_img_width": "320",
+        "slider_img_height": Math.round(item.querySelector('img').getBoundingClientRect().height),
+        "slider_img_width": Math.round(item.querySelector('img').getBoundingClientRect().width),
         "slider_caption": item.querySelector('.caption--ctas').textContent.trim(),
         "slider_url": item.querySelector('a').href,
         "slider_text": item.querySelector('.wpHeadline').textContent.trim()
       }
-    });
+    }).slice(1, -1);
     pageObj['top_banner'] = [topBanner];
     pageObj['rich_content'] = richContent;
     pageObj['slider'] = slider;
@@ -64,12 +64,12 @@ let url = "https://www.michaelkors.com/";
     return pageJson;
   });
 
-  var regex = /(https:\/\/)([\w-]*)(.michaelkors.com\/)/g;
-  cleanHomepageData = JSON.stringify(homePageData, null, 2).replace(regex, '$1www$3')
+  // var regex = /(https:\/\/)([\w-]*)(.michaelkors.com\/)/g;
+  // cleanHomepageData = JSON.stringify(homePageData, null, 2).replace(regex, '$1www$3')
 
   fs.writeFile(
     `./homepage-${moment().format('M-DD-YYYY_H-mm-ss')}.json`,
-    cleanHomepageData,
+    JSON.stringify(homePageData, null, 2),
     err => {
       if (err) {
         return console.log(err);
