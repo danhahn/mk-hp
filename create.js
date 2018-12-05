6945e5dae4f6d90b4c99ad195c5d815b7037451f
Dan Hahn <daniel.hahn@michaelkors.com>
2018-12-05 12:45:39 -0500

    remove async/await

--- create.js
+++ create.js
@@ -4,80 +4,163 @@ const moment = require('moment');
 
 let url = "https://www.michaelkors.com/";
 
-(async () => {
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
-  await page.setViewport({ width: 414, height: 500 });
-  await page.goto(url);
 
-  // get hotel details
-  let homePageData = await page.evaluate(() => {
-    let pageObj = {}
-    let pageJson = {
-      items: [pageObj]
-    };
-    const topBannerDOM = document.querySelector('.flagshipSaleBanner');
-    const topBanner = {
-      "banner_url": topBannerDOM.querySelector('a').href,
-      "banner_img": topBannerDOM.querySelector('[srcset*=mobile]').srcset,
-      "banner_img_height": "358",
-      "banner_img_width": "388",
-      "caption--headline": topBannerDOM.querySelector('h2').textContent.trim(),
-      "font_family": "Caslon",
-      "font_weight": "italic",
-      "captio_text": topBannerDOM.querySelector('.caption--text').textContent.trim(),
-      "caption_ctas": [...topBannerDOM.querySelectorAll('.wpCta:not(.hide-tablet-only) a')].map(item => {
-        return {
-          "cta_text": item.textContent,
-          "cta_url": item.href
-        }
-      }),
-      "bottom_line-text": topBannerDOM.querySelector('.promo--legal').innerHTML.split('<')[0].trim(),
-      "bottom_line_url_text": topBannerDOM.querySelector('.promo--legal a').textContent.trim(),
-      "bottom_line_url": topBannerDOM.querySelector('.promo--legal a').href
-    }
-    const richContent = [...document.querySelectorAll('.mkwp[id]')].map(item => {
-      return {
-        "mmk_url": item.querySelector('a').href,
-        "mmk_img": item.querySelector("[srcset *= 'mobile']").srcset,
-        "mmk_img_width": Math.round(item.querySelector('img').getBoundingClientRect().width),
-        "mmk_img_height": Math.round(item.querySelector('img').getBoundingClientRect().height),
-        "mmk_Headline": item.querySelector('.caption--headline').textContent.trim(),
-        "font_family": [...item.querySelector('.caption').classList].join(' '),
-        "mmk_subtext": item.querySelector('.caption--headline + div').textContent.trim()
-      }
+puppeteer.launch().then(browser => {
+  browser.newPage()
+    .then(page => {
+      page.setViewport({
+        width: 414,
+        height: 500
+      });
+      page.goto(url)
+        .then(resp => page.evaluate((homePageData) => {
+
+          let pageObj = {}
+          let pageJson = {
+            items: [pageObj]
+          };
+          const topBannerDOM = document.querySelector('.flagshipSaleBanner');
+          const topBanner = {
+            "banner_url": topBannerDOM.querySelector('a').href,
+            "banner_img": topBannerDOM.querySelector('[srcset*=mobile]').srcset,
+            "banner_img_height": "358",
+            "banner_img_width": "388",
+            "caption--headline": topBannerDOM.querySelector('h2').textContent.trim(),
+            "font_family": "Caslon",
+            "font_weight": "italic",
+            "captio_text": topBannerDOM.querySelector('.caption--text').textContent.trim(),
+            "caption_ctas": [...topBannerDOM.querySelectorAll('.wpCta:not(.hide-tablet-only) a')].map(item => {
+              return {
+                "cta_text": item.textContent,
+                "cta_url": item.href
+              }
+            }),
+            "bottom_line-text": topBannerDOM.querySelector('.promo--legal').innerHTML.split('<')[0].trim(),
+            "bottom_line_url_text": topBannerDOM.querySelector('.promo--legal a').textContent.trim(),
+            "bottom_line_url": topBannerDOM.querySelector('.promo--legal a').href
+          }
+          const richContent = [...document.querySelectorAll('.mkwp[id]')].map(item => {
+            return {
+              "mmk_url": item.querySelector('a').href,
+              "mmk_img": item.querySelector("[srcset *= 'mobile']").srcset,
+              "mmk_img_width": Math.round(item.querySelector('img').getBoundingClientRect().width),
+              "mmk_img_height": Math.round(item.querySelector('img').getBoundingClientRect().height),
+              "mmk_Headline": item.querySelector('.caption--headline').textContent.trim(),
+              "font_family": [...item.querySelector('.caption').classList].join(' '),
+              "mmk_subtext": item.querySelector('.caption--headline + div').textContent.trim()
+            }
+          });
+          const slider = [...document.querySelector('.carousel').querySelectorAll('figure.promo')].map(item => {
+            return {
+              "slider_img": item.querySelector("[srcset *= 'mobile']").srcset,
+              "slider_img_height": Math.round(item.querySelector('img').getBoundingClientRect().height),
+              "slider_img_width": Math.round(item.querySelector('img').getBoundingClientRect().width),
+              "slider_caption": item.querySelector('.caption--ctas').textContent.trim(),
+              "slider_url": item.querySelector('a').href,
+              "slider_text": item.querySelector('.wpHeadline').textContent.trim()
+            }
+          }).slice(1, -1);
+          pageObj['top_banner'] = [topBanner];
+          pageObj['rich_content'] = richContent;
+          pageObj['slider'] = slider;
+          pageObj['slider_item_count'] = slider.length;
+
+          return pageJson
+        }))
+        .then(homePageData => {
+          fs.writeFile(
+            `./homepage-${moment().format('M-DD-YYYY_H-mm-ss')}.json`,
+            JSON.stringify(homePageData, null, 2),
+            err => {
+              if (err) {
+                return console.log(err);
+              }
+              // console.log(JSON.stringify(homePageData, null, 2));
+              console.log("The file was saved!");
+            }
+          );
+          console.log(JSON.stringify(homePageData, null, 2));
+          browser.close()
+        });
     });
-    const slider = [...document.querySelector('.carousel').querySelectorAll('figure.promo')].map(item => {
-      return {
-        "slider_img": item.querySelector("[srcset *= 'mobile']").srcset,
-        "slider_img_height": Math.round(item.querySelector('img').getBoundingClientRect().height),
-        "slider_img_width": Math.round(item.querySelector('img').getBoundingClientRect().width),
-        "slider_caption": item.querySelector('.caption--ctas').textContent.trim(),
-        "slider_url": item.querySelector('a').href,
-        "slider_text": item.querySelector('.wpHeadline').textContent.trim()
-      }
-    }).slice(1, -1);
-    pageObj['top_banner'] = [topBanner];
-    pageObj['rich_content'] = richContent;
-    pageObj['slider'] = slider;
-    pageObj['slider_item_count'] = slider.length;
-    return pageJson;
-  });
+});
 
-  // var regex = /(https:\/\/)([\w-]*)(.michaelkors.com\/)/g;
-  // cleanHomepageData = JSON.stringify(homePageData, null, 2).replace(regex, '$1www$3')
 
-  fs.writeFile(
-    `./homepage-${moment().format('M-DD-YYYY_H-mm-ss')}.json`,
-    JSON.stringify(homePageData, null, 2),
-    err => {
-      if (err) {
-        return console.log(err);
-      }
-      // console.log(JSON.stringify(homePageData, null, 2));
-      console.log("The file was saved!");
-    }
-  );
-  console.log(JSON.stringify(homePageData, null, 2));
-  await browser.close();
-})();
+
+// (async () => {
+//   const browser = await puppeteer.launch({ headless: true });
+//   const page = await browser.newPage();
+//   await page.setViewport({ width: 414, height: 500 });
+//   await page.goto(url);
+
+//   // get hotel details
+//   let homePageData = await page.evaluate(() => {
+//     let pageObj = {}
+//     let pageJson = {
+//       items: [pageObj]
+//     };
+//     const topBannerDOM = document.querySelector('.flagshipSaleBanner');
+//     const topBanner = {
+//       "banner_url": topBannerDOM.querySelector('a').href,
+//       "banner_img": topBannerDOM.querySelector('[srcset*=mobile]').srcset,
+//       "banner_img_height": "358",
+//       "banner_img_width": "388",
+//       "caption--headline": topBannerDOM.querySelector('h2').textContent.trim(),
+//       "font_family": "Caslon",
+//       "font_weight": "italic",
+//       "captio_text": topBannerDOM.querySelector('.caption--text').textContent.trim(),
+//       "caption_ctas": [...topBannerDOM.querySelectorAll('.wpCta:not(.hide-tablet-only) a')].map(item => {
+//         return {
+//           "cta_text": item.textContent,
+//           "cta_url": item.href
+//         }
+//       }),
+//       "bottom_line-text": topBannerDOM.querySelector('.promo--legal').innerHTML.split('<')[0].trim(),
+//       "bottom_line_url_text": topBannerDOM.querySelector('.promo--legal a').textContent.trim(),
+//       "bottom_line_url": topBannerDOM.querySelector('.promo--legal a').href
+//     }
+//     const richContent = [...document.querySelectorAll('.mkwp[id]')].map(item => {
+//       return {
+//         "mmk_url": item.querySelector('a').href,
+//         "mmk_img": item.querySelector("[srcset *= 'mobile']").srcset,
+//         "mmk_img_width": Math.round(item.querySelector('img').getBoundingClientRect().width),
+//         "mmk_img_height": Math.round(item.querySelector('img').getBoundingClientRect().height),
+//         "mmk_Headline": item.querySelector('.caption--headline').textContent.trim(),
+//         "font_family": [...item.querySelector('.caption').classList].join(' '),
+//         "mmk_subtext": item.querySelector('.caption--headline + div').textContent.trim()
+//       }
+//     });
+//     const slider = [...document.querySelector('.carousel').querySelectorAll('figure.promo')].map(item => {
+//       return {
+//         "slider_img": item.querySelector("[srcset *= 'mobile']").srcset,
+//         "slider_img_height": Math.round(item.querySelector('img').getBoundingClientRect().height),
+//         "slider_img_width": Math.round(item.querySelector('img').getBoundingClientRect().width),
+//         "slider_caption": item.querySelector('.caption--ctas').textContent.trim(),
+//         "slider_url": item.querySelector('a').href,
+//         "slider_text": item.querySelector('.wpHeadline').textContent.trim()
+//       }
+//     }).slice(1, -1);
+//     pageObj['top_banner'] = [topBanner];
+//     pageObj['rich_content'] = richContent;
+//     pageObj['slider'] = slider;
+//     pageObj['slider_item_count'] = slider.length;
+//     return pageJson;
+//   });
+
+//   // var regex = /(https:\/\/)([\w-]*)(.michaelkors.com\/)/g;
+//   // cleanHomepageData = JSON.stringify(homePageData, null, 2).replace(regex, '$1www$3')
+
+//   fs.writeFile(
+//     `./homepage-${moment().format('M-DD-YYYY_H-mm-ss')}.json`,
+//     JSON.stringify(homePageData, null, 2),
+//     err => {
+//       if (err) {
+//         return console.log(err);
+//       }
+//       // console.log(JSON.stringify(homePageData, null, 2));
+//       console.log("The file was saved!");
+//     }
+//   );
+//   console.log(JSON.stringify(homePageData, null, 2));
+//   await browser.close();
+// })();
